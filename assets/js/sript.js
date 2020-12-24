a10d124c07332741c28da77ad80b48944d5145b6
Dax Buratto <daxburatto@gmail.com>
2020-12-23 19:27:22 -0800

    added list items

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,11 +1,23 @@
-var buttonEl = document.querySelector("#save-task")
 var tasksToDoEl = document.querySelector("#tasks-to-do")
+var formEl = document.querySelector("#task-form")
 
 var createTaskHandler = function () {
+    
+    event.preventDefault()
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value
+    var taskTypeInput = document.querySelector("select[name='task-type']").value
+
     var listItemEL = document.createElement("li")
     listItemEL.className = "task-item"
-    listItemEL.textContent = "This is a new task."
+
+    var taskInfoEl = document.createElement("div")
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+    listItemEL.appendChild(taskInfoEl)
+
     tasksToDoEl.appendChild(listItemEL)
 }
 
-buttonEl.addEventListener("click", createTaskHandler)
+formEl.addEventListener("submit", createTaskHandler)
