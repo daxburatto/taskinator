ba494fd9b646d3accf767d9b104c2e7ee495a351
Dax Buratto <daxburatto@gmail.com>
2020-12-26 17:17:46 -0800

    added drag and drop features

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -49,6 +49,8 @@ var createTaskEl = function (taskDataObj) {
 
     listItemEL.setAttribute("data-task-id", taskIdCounter)
 
+    listItemEL.setAttribute("draggable", "true")
+
     var taskInfoEl = document.createElement("div")
     taskInfoEl.className = "task-info"
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"
@@ -145,8 +147,58 @@ var taskStatusChangeHandler = function (event) {
     }
 }
 
+var dragTaskHandler = function (event) {
+    if (event.target.matches("li.task-item")) {
+        var taskId = event.target.getAttribute("data-task-id")
+        event.dataTransfer.setData("text/plain", taskId)
+    }
+}
+
+var dropZoneDragHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list")
+    if (taskListEl) {
+        event.preventDefault()
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed")
+    }
+}
+
+var dropTaskHandler = function (event) {
+    event.preventDefault()
+    var id = event.dataTransfer.getData("text/plain")
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']")
+    var dropZoneEl = event.target.closest(".task-list")
+    var statusType = dropZoneEl.id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']")
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0        
+    }
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1
+    }
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2
+    }
+    dropZoneEl.removeAttribute("style")
+    dropZoneEl.appendChild(draggableElement)
+}
+
+var dragLeaveHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list")
+    if (taskListEl) {
+        taskListEl.removeAttribute("style")
+    }
+}
+
 pageContentEL.addEventListener("click", taskButtonHandler)
 
 formEl.addEventListener("submit", taskFormHandler)
 
 pageContentEL.addEventListener("change", taskStatusChangeHandler)
+
+pageContentEL.addEventListener("dragstart", dragTaskHandler)
+
+pageContentEL.addEventListener("dragover", dropZoneDragHandler)
+
+pageContentEL.addEventListener("dragleave", dragLeaveHandler)
+
+pageContentEL.addEventListener("drop", dropTaskHandler)
